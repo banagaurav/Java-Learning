7d9d15cce9c2a0713288e818cc64f6035a39684f
banagaurav <gauravbana840@gmail.com>
2024-09-18 07:20:02 +0545

    add Applet

--- Chapter02/Grid Layout/src/GridLayoutExample.java
+++ Chapter02/Grid Layout/src/GridLayoutExample.java
@@ -13,22 +13,22 @@ public class GridLayoutExample{
             JButton btn2 = new JButton("2");
             JButton btn3 = new JButton("3");
             JButton btn4 = new JButton("4");
-//            JButton btn5 = new JButton("5");
-//            JButton btn6 = new JButton("6");
-//            JButton btn7 = new JButton("7");
-//            JButton btn8 = new JButton("8");
-//            JButton btn9 = new JButton("9");
+            JButton btn5 = new JButton("5");
+            JButton btn6 = new JButton("6");
+            JButton btn7 = new JButton("7");
+            JButton btn8 = new JButton("8");
+            JButton btn9 = new JButton("9");
 
         //adding buttons to the frame
             frameObj.add(btn1);
             frameObj.add(btn2);
             frameObj.add(btn3);
             frameObj.add(btn4);
-//            frameObj.add(btn5);
-//            frameObj.add(btn6);
-//            frameObj.add(btn7);
-//            frameObj.add(btn8);
-//            frameObj.add(btn9);
+            frameObj.add(btn5);
+            frameObj.add(btn6);
+            frameObj.add(btn7);
+            frameObj.add(btn8);
+            frameObj.add(btn9);
 
             frameObj.setLayout(new GridLayout(2,2));
 
