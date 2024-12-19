6c532774e30b968359eeb10f903c99d509bf0a42
banagaurav <gauravbana840@gmail.com>
2024-12-19 22:43:39 +0545

    add MenuExample

---
+++ Calculator/MenuExample.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+class MenuExample{
+    JMenu menu,submenu;
+    JMenuItem i1,i2,i3,i4;
+    public MenuExample() {
+    JFrame f = new JFrame("Menu and MenuItem Example");
+    JMenuBar mb = new JMenuBar();
+    menu= new JMenu("Menu");
+    submenu = new JMenu("Sub Menu");
+    }
+}
