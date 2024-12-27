d6a515fbe2614ab5b6a28d898e16128ce33f0260
banagaurav <gauravbana840@gmail.com>
2024-12-27 14:37:41 +0545

    fix program AEvent

--- Example/AEvent.java
+++ Example/AEvent.java
@@ -1,29 +1,33 @@
+
 import java.awt.*;
 import java.awt.event.*;
 
-public class AEvent extends Frame implements  ActionListener {
-    TextField tf;
-    public AEvent() {
-    //create components
-    tf = new TextField();
-    tf.setBounds(60,50,170,20);
-    Button b = new Button("Click me");
-    b.setBounds(100,120,80,30);
 
-    //Resgister Listener
-    b.addActionListener(this); //passing current instance
+class AEvent extends Frame implements ActionListener{
+ TextField tf;
 
-    //add components & setSize , Layout and visibility
-    add(tf);
-    add(b);
-    setSize(300,300);
-    setVisible(true);
-}
-public void actionPerformed(ActionEvent e)
-{
-    tf.setText("Welcome");
-}
-    public static void main(String[] args) {
-    new AEvent();    
+    public AEvent(){
+        tf = new TextField();
+        tf.setBounds(60,50,170,20);
+        Button b = new Button("Click me");
+        b.setBounds(100,120,80,30);
+
+        b.addActionListener(this); //passing cuent instance
+
+        add(tf);
+        add(b);
+        setLayout(null);
+        setVisible(true);
     }
+
+    public void actionPerformed(ActionEvent e){
+        tf.setText("Welcome");
+    }
+ 
+    public static void main(String[] args) {
+        new AEvent();
+    }
+
+    
+
 }
--- Example/PopupExample.java
+++ Example/PopupExample.java
@@ -1,10 +1,10 @@
 
 import java.awt.*;
 
-class PopupExample{
+class PopupExample extends Frame{
 
     public PopupExample() {
-        final Frame f = new Frame("Popup MEnu Example");
+        final Frame f = new Frame("Popup Menu Example");
         final PopupMenu pop = new PopupMenu("Edit");
         MenuItem Copy = new MenuItem("Copy");
         Copy.setActionCommand("Copy");
